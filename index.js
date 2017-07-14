c246b9b8f54fb450874b273b99bdfc99f999ad86
Kraigo <kraigo@live.com>
2017-07-15 00:34:40 +0300

    connect

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-mongoose.createConnection(process.env.MONGO_DB);
+mongoose.connect(process.env.MONGO_DB);
 mongoose.connection.on('error', console.error);
 
 
