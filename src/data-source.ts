c185d65f6dbffc11c422bb2de4f68fc4dc8831e1
Kraigo <kraigo@live.com>
2022-05-31 23:47:55 +0300

    fix log

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,12 +1,13 @@
 import { dirname } from "path";
 import { DataSource } from "typeorm";
 import { QuestionEntity, QuizEntity, UserEntity } from "./entities";
+import { isDebug } from "./utils";
 const root = dirname(require.main.filename);
 
 export const AppDataSource = new DataSource({
     type: "sqlite",
     database: `${root}/../data.sqlite`,
     synchronize: true,
-    logging: true,
+    logging: isDebug,
     entities: [UserEntity, QuizEntity, QuestionEntity]
 });
